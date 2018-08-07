c4aa986f66dfecbd2b339a25d81dab84ff724301
likhitaaluru <likhitaaluru@gmail.com>
2018-08-07 14:46:32 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -2,10 +2,13 @@
 
 puts "Hi, you've been invited to a party! What is your name?"
 guest_name=gets.chomp
-
+puts "What is the party_name?"
 party_name=gets.chomp
+puts "What is the date?"
 date = gets.chomp
+puts "What time is the party at?"
 time = gets.chomp
+puts"Who is the host?"
 host_name = gets.chomp
 
 
